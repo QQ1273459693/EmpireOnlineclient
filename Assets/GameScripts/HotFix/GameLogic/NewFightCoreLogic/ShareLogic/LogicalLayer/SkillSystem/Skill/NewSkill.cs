b22a7726f3eef5dc7dddac85f53b5a5e1e1b52bb
QQ1273459693 <1273459693@QQ.COM>
2023-11-20 00:40:47 +0800

    11.20

---
+++ Assets/GameScripts/HotFix/GameLogic/NewFightCoreLogic/ShareLogic/LogicalLayer/SkillSystem/Skill/NewSkill.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TEngine;
+using UnityEngine;
+
+public class NewSkill
+{
+
+    public int Skillid { get; private set; }
+
+    private HeroLogic mSkillOwner;//技能拥有者
+
+    private SkillConfig mSkillCfg;
+
+    private LogicObject mSkillTarget;
+
+    private bool mIsNormalAtk; //是否是普通攻击
+
+    public NewSkill(int skillid, LogicObject skillOwner, bool isNoramlAtk)
+    {
+        Skillid = skillid;
+        mSkillOwner = (HeroLogic)skillOwner;
+        mSkillCfg = SkillConfigConter.LoadSkillConfig(skillid);
+        mIsNormalAtk = isNoramlAtk;
+        if (mSkillCfg == null)
+        {
+            Debuger.LogError("技能配置不存在 技能id：" + skillid);
+        }
+
+    }
+}
